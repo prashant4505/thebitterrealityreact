f946b7f2ea15962db7fee81f412b314643c8c641
Prashant Singh <prashant.s@svinfotech.net>
2025-02-25 16:07:24 +0530

    Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
         <Link className="nav-link" to="/">Home</Link>
         <Link className="nav-link" to="/blogs">Blogs</Link>
         <Link className="nav-link" to="/jokes">Jokes</Link>
-        <Link className="nav-link" to="/deep-thoughts">Deep Thoughts</Link>
+        <Link className="nav-link" to="/deep-thoughts">Thoughts</Link>
         <Link className="nav-link" to="/about">About Us</Link>
         <Link className="nav-link" to="/contact">Contact</Link>
 
