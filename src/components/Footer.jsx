fd58dbd0a24d1062d4d8bd882ac1aba7f7c0d34b
Prashant Singh <prashant.s@svinfotech.net>
2025-03-03 17:54:12 +0530

    updated css and make footer date dynamic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <p>© 2025 The Bitter Reality – because life’s too short for fake smiles.</p>
+            <p>© {currentYear} The Bitter Reality – because life’s too short for fake smiles.</p>
             <p><Link className="nav-link" to="/privacy">Privacy Policy</Link> | <Link className="nav-link" to="/terms">Terms of Use</Link></p>
         </footer>
     )
